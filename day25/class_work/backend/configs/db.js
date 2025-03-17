92b2b9864d6de4a493aef7d8ed053fde516c1d66
Santanumandal345 <Santanumandal957@gmail.com>
2025-03-18 02:10:30 +0530

    Assiment

--- day25/class_work/backend/configs/db.js
+++ day25/class_work/backend/configs/db.js
@@ -4,31 +4,31 @@
 
 
 
-const mongooes=require("mongoose")
-const connetDB=mongooes.connect( "mongodb+srv://Santanu123:santanu@cluster0.spcu7.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0")
+// const mongooes=require("mongoose")
+// const connetDB=mongooes.connect( "mongodb+srv://Santanu123:santanu@cluster0.spcu7.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0")
 
 
 
-module.exports={
-    connetDB
-};
-
-
-
-
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect("mongodb+srv://Santanu123:Santanu@123@cluster0.spcu7.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0");
-//         console.log("MongoDB connected successfully");
-//     } catch (error) {
-//         console.error("MongoDB connection error:", error);
-//         process.exit(1);
-//     }
+// module.exports={
+//     connetDB
 // };
 
-// module.exports = { connectDB };
+
+
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://Santanu123:santanu@cluster0.spcu7.mongodb.net/myDB?retryWrites=true&w=majority&appName=Cluster0");
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
+
+module.exports = { connectDB };
 
 
 
