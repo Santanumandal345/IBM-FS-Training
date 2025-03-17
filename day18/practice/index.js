f49698f5fc9dc8959bf114eb84d2e53e2b3892f8
Santanumandal345 <Santanumandal957@gmail.com>
2025-03-18 01:31:05 +0530

    Assiment

---
+++ day18/practice/index.js
@@ -0,0 +1,21 @@
+let lastUpdateTime = 
+Date.now();
+let colorUpdateTime = 0.0;
+let config={
+    SIM_RESOLUTION: 128,
+    DYE_RESOLUTION:1440,
+    CAPTURE_RESOLUTION:512,
+    DENSITY ,DISSIPATION: 3.5,
+    VELOCITY_DISSSIPATION:2,
+    PRESSURE:0.1,
+    PRESSURE_ITERATIONS:20,
+    CURL:10,
+    SPLAT_RADIUS:0.5,
+    SPLAT_FORCE:6000,
+    SHADING:true,
+    COLOR_UPDATE_SPEED:10,
+    PAUSED:false,
+    BACK ,COLOR:{ r: 0, g: 0, b: 0 },
+    TRANSPARENT:true,
+
+}
