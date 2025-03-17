d6e22f371b9a9ac93dc5c7d4a45d11b360c8331a
Santanumandal345 <Santanumandal957@gmail.com>
2025-03-18 01:30:36 +0530

    Assiment

---
+++ day20/class_work/index.js
@@ -0,0 +1,12 @@
+const os= require("os");
+console.log(os.hostname());
+// console.log(os.machine());
+// console.log(os.platform());
+// // console.log(os.cpus());
+// console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.userInfo());
+// console.log(os.version());
+// console.log(os.uptime());
+// console.log(os.type());
+console.log(os.freemem());
